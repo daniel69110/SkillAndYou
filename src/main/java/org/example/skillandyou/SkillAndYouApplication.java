5aed5f576e56ee1039906ea0907c5953a60b2c5c
Daniel-C <danielcourot@hotmail.com>
2025-12-26 17:18:38 +0100

    first commit

---
+++ src/main/java/org/example/skillandyou/SkillAndYouApplication.java
@@ -0,0 +1,13 @@
+package org.example.skillandyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillAndYouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillAndYouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/skillandyou/SkillAndYouApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.skillandyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillAndYouApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
